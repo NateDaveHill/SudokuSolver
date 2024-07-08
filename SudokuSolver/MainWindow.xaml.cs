10ab3846dc4199cae1b1658961d3ed3dd21cc9ea
Nathan David Hill <nathan.hill@gmx.at>
2024-07-07 21:59:33 -0400

    processing the column and trying to see which number is missing

--- SudokuSolver/MainWindow.xaml.cs
+++ SudokuSolver/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,6 +18,7 @@ namespace SudokuSolver
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int Size = 9;
 
 
         public MainWindow()
@@ -55,19 +57,28 @@ namespace SudokuSolver
 
         private void BtnSolve_OnClick(object sender, RoutedEventArgs e)
         {
+            List<int> currentColumn = new List<int>();
+            int itemInColumn = 0;
+
             var items = SudokuBoard.ItemsSource as IEnumerable<GridData>;
             if (items != null)
             {
                 foreach (var item in items)
                 {
-                    var propertyValue = item.One;
+                    itemInColumn = item.One;
+                    currentColumn.Add(item.One);
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    var result = Enumerable.Range(0, 3).Except(currentColumn);
                 }
             }
         }
 
         private void BtnCancel_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            this.Close();
         }
 
         private void BtnRandomPopulate_OnClick(object sender, RoutedEventArgs e)
