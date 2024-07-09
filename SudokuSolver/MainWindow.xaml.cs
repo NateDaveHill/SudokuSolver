bd2885edb50dada06ef29b13c3ca5b8511e5bf3a
Nathan David Hill <nathan.hill@gmx.at>
2024-07-09 09:09:28 -0400

    successfully update the missing number in the first column > now trying to iterate each column dynamically

--- SudokuSolver/MainWindow.xaml.cs
+++ SudokuSolver/MainWindow.xaml.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SudokuSolver
 {
@@ -19,35 +22,36 @@ namespace SudokuSolver
     public partial class MainWindow : Window
     {
         private const int Size = 9;
+        List<GridData> boardData = new List<GridData>();
+
 
 
         public MainWindow()
         {
             InitializeComponent();
             SudokuBoard.ItemsSource = LoadCollectionData();
-            
         }
 
         private List<GridData> LoadCollectionData()
         {
-            List<GridData> boardData = new List<GridData>();
+
             boardData.Add(new GridData()
             {
                 One = 1,
                 Two = 2,
                 Three = 3
             });
-            
+
             boardData.Add(new GridData()
             {
                 One = 0,
                 Two = 2,
                 Three = 1
             });
-            
+
             boardData.Add(new GridData()
             {
-                One = 0,
+                One = 2,
                 Two = 3,
                 Three = 2
             });
@@ -57,13 +61,31 @@ namespace SudokuSolver
 
         private void BtnSolve_OnClick(object sender, RoutedEventArgs e)
         {
+            List<GridData> updatedNumbers = new List<GridData>();
             List<int> currentColumn = new List<int>();
-            int itemInColumn = 0;
             IEnumerable<int> missingNumbers = null;
+            int itemInColumn = 0;
+
 
             var numbers = SudokuBoard.ItemsSource as IEnumerable<GridData>;
             if (numbers != null)
             {
+
+                foreach (var item in boardData)
+                {
+                    Type type = typeof(GridData);
+
+                    PropertyInfo[] properties = type.GetProperties();
+
+                    foreach (var property in properties)
+                    {
+                        string propertyName = property.Name;
+                        int propertyValue = Convert.ToInt32(property.GetValue(item));
+
+                    }
+                }
+
+
                 foreach (var item in numbers)
                 {
                     itemInColumn = item.One;
@@ -75,17 +97,24 @@ namespace SudokuSolver
                     missingNumbers = Enumerable.Range(1, 3).Except(currentColumn);
                 }
 
-                foreach (var item in numbers)
+
+                foreach (var number in numbers)
                 {
-                    if (item.One == 0)
+                    if (number.One == 0)
                     {
                         foreach (var missingNumber in missingNumbers)
                         {
-                            item.One = missingNumber;
+                            updatedNumbers.Add(new GridData { One = missingNumber, Two = number.Two, Three = number.Three });
                         }
-
+                    }
+                    else
+                    {
+                        updatedNumbers.Add(number);
                     }
                 }
+
+                SudokuBoard.ItemsSource = updatedNumbers;
+
             }
         }
 
